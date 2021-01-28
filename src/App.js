9cd3a4869dbeb577719f9f6f14b4b53b032ce9fc
Larissa Evaldt <lari.justo@gmail.com>
2021-01-28 16:35:18 +0000

    Section 8 React state patterns Lottery Exercise

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 // change this import according to which exercise to render
-import RollDice from './07_Dice_Exercise/RollDice';
+import Lottery from './08_Lottery_Exercise/Lottery';
 
 class App extends Component {
   render() {
     return (
     <div className="App">
       {/* change the component according to the exercise to render */}
-      <RollDice />
+        <Lottery />
+        <Lottery title="Mini Daily" maxNum={10} numBalls={4} />
     </div>
     );
   }
