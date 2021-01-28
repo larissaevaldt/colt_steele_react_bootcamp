22640d8b0b74947a802e04558310ada0919f6717
Larissa Evaldt <lari.justo@gmail.com>
2021-01-28 15:42:40 +0000

    Section 6 Introducing State Clicker Exercise

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 // change this import according to which exercise to render
-import Pokegame from './05_Pokedex_Exercise/Pokegame';
+import Clicker from './06_Clicker_Exercise/Clicker';
 
 class App extends Component {
   render() {
     return (
     <div className="App">
       {/* change the component according to the exercise to render */}
-      <Pokegame />
+      <Clicker />
     </div>
     );
   }
