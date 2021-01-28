5e7db0277280d3f11d3654499d871d673cf4401a
Larissa Evaldt <lari.justo@gmail.com>
2021-01-28 16:28:16 +0000

    React State Dice Exercise

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 // change this import according to which exercise to render
-import Clicker from './06_Clicker_Exercise/Clicker';
+import RollDice from './07_Dice_Exercise/RollDice';
 
 class App extends Component {
   render() {
     return (
     <div className="App">
       {/* change the component according to the exercise to render */}
-      <Clicker />
+      <RollDice />
     </div>
     );
   }
