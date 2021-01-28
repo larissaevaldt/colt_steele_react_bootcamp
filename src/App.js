c8391e9e993489a582528508b07405c80a7c397c
Larissa Evaldt <lari.justo@gmail.com>
2021-01-28 15:24:52 +0000

    Section 5 Pokedex Project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+// change this import according to which exercise to render
+import Pokegame from './05_Pokedex_Exercise/Pokegame';
 
 class App extends Component {
   render() {
     return (
     <div className="App">
-      
+      {/* change the component according to the exercise to render */}
+      <Pokegame />
     </div>
     );
   }
