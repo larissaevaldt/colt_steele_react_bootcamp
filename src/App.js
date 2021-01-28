e72ae5938497cf5a8c0ba80f1b408e4abc5668e0
Larissa Evaldt <lari.justo@gmail.com>
2021-01-28 17:36:11 +0000

    Section 11 Hangman Exercise

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 // change this import according to which exercise to render
-import BoxContainer from './09_State_Exercises/Color_Boxes_Exercise/BoxContainer';
+import Hangman from './11_Hangman_Exercise/Hangman';
 
 class App extends Component {
   render() {
@@ -10,7 +10,7 @@ class App extends Component {
       {/* change the component according to the exercise to render */}
         {/* <Lottery />
         <Lottery title="Mini Daily" maxNum={10} numBalls={4} /> */}
-        <BoxContainer />
+        <Hangman />
     </div>
     );
   }
