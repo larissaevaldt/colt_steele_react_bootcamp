b6adb0fe007514ae4169ff9032df892470a5a31d
Larissa Evaldt <lari.justo@gmail.com>
2021-02-02 00:32:09 +0000

    Section 19 Dad Jokes Exercise

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 // change this import according to which exercise to render
-import Deck from './18_DeckOfCards_Exercise/Deck';
+import JokeList from './19_DadJokes_Exercise/JokeList';
 
 class App extends Component {
+    
   render() {
     return (
     <div className="App">
       {/* change the component according to the exercise to render */}
         {/* <Lottery />
         <Lottery title="Mini Daily" maxNum={10} numBalls={4} /> */}
-        <Deck />
+        <JokeList />
     </div>
     );
   }
