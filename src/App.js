873ca3e6bd74ead37f1cb3e429c0267d7df51210
Larissa Evaldt <lari.justo@gmail.com>
2021-02-04 18:32:54 +0000

    Introduction to react hooks with useState

--- src/App.js
+++ src/App.js
@@ -1,56 +1,17 @@
 import React, { Component } from 'react';
-import Navbar from './23_DogApp_Exercise/Navbar';
-import Routes from './23_DogApp_Exercise/Routes';
-import whiskey from './23_DogApp_Exercise/img/whiskey.jpg'
-import tubby from './23_DogApp_Exercise/img/tubby.jpg'
-import hazel from './23_DogApp_Exercise/img/hazel.jpg'
+
 import './App.css';
 // change this import according to which exercise to render
-
+import CounterClass from './32_Introducing_React_Hooks/CounterClass';
+import CounterHooks from './32_Introducing_React_Hooks/CounterHooks';
 class App extends Component {
-    static defaultProps = {
-        dogs: [
-        {
-            name: "Whiskey",
-            age: 5,
-            src: whiskey,
-            facts: [
-            "Whiskey loves eating popcorn.",
-            "Whiskey is a terrible guard dog.",
-            "Whiskey wants to cuddle with you!"
-            ]
-        },
-        {
-            name: "Hazel",
-            age: 3,
-            src: hazel,
-            facts: [
-            "Hazel has soooo much energy!",
-            "Hazel is highly intelligent.",
-            "Hazel loves people more than dogs."
-            ]
-        },
-        {
-            name: "Tubby",
-            age: 4,
-            src: tubby,
-            facts: [
-            "Tubby is not the brightest dog",
-            "Tubby does not like walks or exercise.",
-            "Tubby loves eating food."
-            ]
-        }
-        ]
-    };
     
   render() {
     
     return (
         <div className="App">
-            <Navbar dogs={this.props.dogs} />
-            <div className="container">
-                <Routes dogs={this.props.dogs} />
-            </div>
+            <CounterClass />
+            <CounterHooks />
         </div>
     );
   }
